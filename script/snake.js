71037ae375cb6c98969b58925c12a029603ac29a
Name <email@example.ru>
2022-11-08 14:49:15 +0500

    added partly modify for Snake

--- script/snake.js
+++ script/snake.js
@@ -5,11 +5,12 @@ export default class Snake {
         this.color = "green";
         this.cellSize = cellSize;
         this.length = 2 * cellSize;
+        //начальные координаты
         this.x = fieldSize/2 - cellSize;
         this.y = 75 + fieldSize/2;
-        this.dX = cellSize; //направление по оси X
-		this.dY = 0; //направление по оси Y
-        // postion = [150,150]
+        this.dX = cellSize; //направление по оси X, изначально змейка идет направо
+		this.dY = 0; //направление по оси Y, изначально не движется по вертикали
+        this.snakeCells = [];
     }
 
     defeat(result) {
@@ -29,23 +30,40 @@ export default class Snake {
 
     modify() {
         //логика(условия) изменения змейки
+        this.x += this.dX;
+		this.y += this.dY;
+
+        //действия при достижении границы поля
+        if (this.x < 75) {
+			this.x = (this.cellSize * 10 + 75) - this.cellSize;
+		} else if ( this.x >= this.cellSize * 10 + 75 ) {
+			this.x = 75;
+		}
+	
+		if (this.y < 175) {
+			this.y = (this.cellSize * 10 + 175) - this.cellSize;
+		} else if ( this.y >= this.cellSize * 10 + 175 ) {
+			this.y = 175;
+		}
+
+        this.snakeCells.unshift( { x: this.x, y: this.y } );
     }
 
-    move(cellSize) {
+    move() {
         // управление змейкой
         // обработка нажатия кнопок клавиатуры
         document.addEventListener("keydown",  (event) => {
-			if ( event.code == "KeyW" ) {
-				this.dY = -cellSize;
+			if (( event.code == "KeyW" || event.code == "38") && (this.dY === 0)) {
+				this.dY = -this.cellSize; //вверх
 				this.dX = 0;
-			} else if ( event.code == "KeyA" ) {
-				this.dX = -cellSize;
+			} else if ( (event.code == "KeyA" || event.code == "37") && (this.dX === 0)) {
+				this.dX = -this.cellSize; //налево
 				this.dY = 0;
-			} else if ( event.code == "KeyS" ) {
-				this.dY = cellSize;
+			} else if (( event.code == "KeyS" || event.code == "40") && (this.dY === 0)) {
+				this.dY = this.cellSize; //вниз
 				this.dX = 0;
-			} else if ( event.code == "KeyD" ) {
-				this.dX = cellSize;
+			} else if (( event.code == "KeyD" || event.code == "39" ) && (this.dX === 0)) {
+				this.dX = this.cellSize; //направо
 				this.dY = 0;
 			}
 		});
