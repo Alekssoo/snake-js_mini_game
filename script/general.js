6cd2da2d1bc356f0172e24ce3beb5e62cec7ad5e
Name <email@example.ru>
2022-11-10 22:19:09 +0500

    fixed start of game events

--- script/general.js
+++ script/general.js
@@ -35,7 +35,7 @@ export default class General {
     }
 
     startGame() {
-        new GameLoop(this.modify(), this.generate())
+        new GameLoop(this.modify(), this.generate());
         canvas.removeEventListener("click", this.startGame());
         
     }
